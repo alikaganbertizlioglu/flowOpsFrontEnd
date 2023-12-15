e0fc917f4b94155aa2cfaf98467ddf0c4cd6a6d1
Ali Kağan Bertizlioğlu <alikaganbertizlioglu@gmail.com>
2023-12-15 22:58:50 +0300

    register component extended

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { NgbCalendar, NgbDatepickerModule, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+	today = inject(NgbCalendar).getToday();
 
+	model: NgbDateStruct | undefined;
+	date: { year: number; month: number; } | undefined;
 }
